b230e9e62a3603d91affd2776854c8975895a1e2
AliElDani <alidani03@hotmail.com>
2015-08-02 12:23:45 -0600

    Added user class.

---
+++ TaskManager/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// The first name of the user
+        /// </summary>
+        private string _firstName;
+
+        /// <summary>
+        /// The last name of the user
+        /// </summary>
+        private string _lastName;
+
+        /// <summary>
+        /// The personal number of the user
+        /// </summary>
+        private string _workPhoneNumber;
+
+    }
+}
