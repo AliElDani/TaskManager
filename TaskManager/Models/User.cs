024406be969e52b4d28bca5d8f313c0f2cd56432
AliElDani <alidani03@hotmail.com>
2015-08-06 16:55:37 -0600

    Added resources and view windows to begin work

--- TaskManager/Models/User.cs
+++ TaskManager/Models/User.cs
@@ -9,9 +9,19 @@ namespace TaskManager.Models
     public class User
     {
         /// <summary>
+        /// The user id associated in the database
+        /// </summary>
+        private int _userId;
+
+        /// <summary>
         /// The first name of the user
         /// </summary>
         private string _firstName;
+
+        /// <summary>
+        /// The email of the user
+        /// </summary>
+        private string _email;
 
         /// <summary>
         /// The last name of the user
@@ -22,6 +32,23 @@ namespace TaskManager.Models
         /// The personal number of the user
         /// </summary>
         private string _workPhoneNumber;
+
+        /// <summary>
+        /// The user object assigned to each task
+        /// </summary>
+        /// <param name="userId">the user id associated to the db</param>
+        /// <param name="firstName">the first name of the user</param>
+        /// <param name="lastName">the last name of the user</param>
+        /// <param name="workPhoneNumber">the work number of the user</param>
+        public User(int userId, string firstName, string lastName,string email, string workPhoneNumber)
+        {
+            this._userId = userId;
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._workPhoneNumber = workPhoneNumber;
+            this._email = email;
+        }
+
 
     }
 }
